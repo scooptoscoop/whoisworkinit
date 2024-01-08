d9aa16109c6096018687cc239f5b5c0349ed8563
MC <>
2024-01-08 16:20:51 -0600

    new images

--- list.py
+++ list.py
@@ -6,9 +6,7 @@ files = glob.glob('imgs/*')
 
 # Sort the list in ascending order
 files.sort()
-
 # Format the sorted list as a JavaScript array
 js_array = "var imgs = ['" + "','".join(files) + "'];"
-
 # Print the JavaScript array
 print(js_array)
